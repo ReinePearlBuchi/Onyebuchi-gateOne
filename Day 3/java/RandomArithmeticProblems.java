c7ff5e27134056ddcfdd65ca1ac45cac89fe8301
Ekwutozia Onyebuchi Maureen <165788844+ReinePearlBuchi@users.noreply.github.com>
2024-05-08 13:24:59 +0100

    gateOne

---
+++ Day 3/java/RandomArithmeticProblems.java
@@ -0,0 +1,86 @@
+import java.Random;
+import java.util.Scanner;
+public class RandomArithmeticProblems{
+	public static void main(String[] args){
+	Scanner scanner = new Scanner(System.in);
+
+
+
+	System.out.print("Welcome! What is your name? ");
+	String name = scanner.nextLine();
+
+	System.out.print("Select level");
+	String level = scanner.nextLine();
+	 
+	if(level = "Easy"){
+		System.out.print("Here Goes");
+	
+	else:
+		(level = "Hard"){
+		System.out.print("Alright then");
+	}
+	
+	System.out
+	
+	int questions = 10;
+	for(int count = 0; count <= questions; count++){
+		
+	Switch(	questions)
+		case 1 -> System.out.print("Find the sum of 56 and 78");
+				break,
+
+		case 2 -> System.out.print("Find the difference of 56 and 78");
+				break
+
+		case 3 -> System.out.print("multiply 56 and 78 by 2");
+				break,
+		
+		case 4 -> System.out.print("multiply 1000 by 2");
+				break,
+
+		case 5 -> System.out.print(" whats 56 and 78 by 2");
+				break,
+
+		case 6 -> System.out.print("multiply 1000 by 100");
+				break
+
+		case 7 -> System.out.print("multiply 56 and 78 by 2");
+				break;
+
+		case 8 -> System.out.print("multiply 2 numbers by 10000");
+				break;
+
+		case 9 -> System.out.print("divide 56 and 78 by 2");
+				break;
+
+		case 10 -> System.out.print("add numbers 78 and 10000");
+				break;
+
+		default;
+		
+		Switch(Hard)
+			case 1
+
+
+		
+	for(int count = 0; count <= level.length; count++){
+
+		if(userInput = "A"){
+			System.out.print("right");
+	
+			else(userInput = "B"){
+				System.out.print("wrong");
+
+			}
+		}
+	}	
+	
+		userInput = scanner.nextLine();
+                    if (userInput.equalsIgnoreCase("a")) {
+                        countA4++;
+                    
+		 else (countB4)
+			}
+	
+	}
+}
