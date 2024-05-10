003b3bd699e03ded8d848cc8add0a0133842e6d4
Ekwutozia Onyebuchi Maureen <165788844+ReinePearlBuchi@users.noreply.github.com>
2024-05-10 13:04:48 +0100

    gateOne

---
+++ Day 5/java/AutomatedTellerMachine.java
@@ -0,0 +1,59 @@
+import java.Array;
+import java.util.Scanner;
+	public class AutomatedTellerMachine{
+	public static void main(String[] args){
+
+		Scanner scanner = new Scanner();
+
+	
+		int a = 1234;
+		String card1 = integer.toString(a);
+		System.out.print(card1);
+
+		int b = 5678;
+		String card2 = integer.toString(b);
+		System.out.print(card2);
+
+		int c = 9012;
+		String card3 = integer.toString(c);
+		System.out.print(card3);
+
+		int d = 3456;
+		String card4 = integer.toString(d);
+		System.out.print(card4);
+
+		}
+	
+	public int ReverseInt(int number){
+		int result = 0;
+		while (num > 0)
+		{
+
+		result = result * 10 + num % 10;
+		num /= 10;
+		}
+
+		return result;
+	
+
+	int number;
+
+	do{
+		System.out.println("Please enter a 16 digit number ");
+
+	while(specialNumber.nextInt()){
+		System.out.println("false Thats not valid ");
+		specialNumber.nextInt();
+	}
+	number = specialNumber.nextInt();
+	}
+	
+	while(number < = 0);
+	System.out.println(True this number is valid);
+
+
+
+		
+	}
+
+}
---
+++ Day 5/python/AutomatedTellerMachine.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a four digit number"))
+b = int(input("Enter a four digit number"))
+c = int(input("Enter a four digit number"))
+d = int(input("Enter a four digit number"))
+
+if a == 1234
+	return number
+
+
+
