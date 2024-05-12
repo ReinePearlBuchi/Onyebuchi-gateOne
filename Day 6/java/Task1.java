46231369bb4443824d86576746a7a074395014a6
Ekwutozia Onyebuchi Maureen <165788844+ReinePearlBuchi@users.noreply.github.com>
2024-05-12 20:16:18 +0100

    gateOne

---
+++ Day 6/java/Task1.java
@@ -0,0 +1,30 @@
+
+import java.time.LocalDate;
+import class Task1{
+	public static void main(String[] args){
+	
+	Scanner scanner = new Scanner(System.in);
+	System.out.print("Enter date of birth");
+
+	String age = scanner.nextLine();
+
+	int total = searchAge(age);
+	System.out.print("You are" + total + "years old");
+
+	}
+
+	
+	public static int searchAge(String numbers){
+	
+	char charAtZero = numbers.charAt(0);
+
+
+
+
+	}
+
+	}
+
+
+
+}
