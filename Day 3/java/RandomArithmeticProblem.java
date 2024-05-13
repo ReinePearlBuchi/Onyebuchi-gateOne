d7f8a272ddf197925347dac584b0a9b13bf115c3
Ekwutozia Onyebuchi Maureen <165788844+ReinePearlBuchi@users.noreply.github.com>
2024-05-13 13:12:42 +0100

    gateOne

---
+++ Day 3/java/RandomArithmeticProblem.java
@@ -0,0 +1,56 @@
+import Random.Range;
+import MathSq;
+import java.util.Scanner;
+
+public class RandomArithmeticProblems{
+	Public static void main(String[] args){
+
+	Scanner scanner = new Scanner(System.in);
+
+	System.out.print("What is your name? ");
+	String name = scanner.nextLine();
+
+	System.out.print("Choose a level");
+	
+	
+	
+	
+	Switch 
+		case 1 = System.out.print("");
+
+		case 2 = System.out.print("");
+		
+		case 3 = System.out.print("");
+		
+		case 4 = System.out.print("");
+		
+		case 5 = System.out.print("");
+
+		case 6 = System.out.print("");
+		
+		case 7 = System.out.print("");
+		
+		case 8 = System.out.print("");
+
+
+
+
+	if(userInput = "A"){
+		System.out.print("right");
+	
+	else(userInput = "B"){
+		System.out.print("wrong");
+
+	
+		for(int count )
+
+	
+
+
+	
+
+
+	}
+
+
+}
---
+++ Day 3/java/RandomArithmeticProbrlem.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class RandomArithmeticProblems{
+	public static void main(String[] args){
+	Scanner scanner = new Scanner(System.in);
+
+	System.out.print("Welcome! What is your name? ");
+	String name = scanner.nextLine();
+
+	System.out.print("Select level");
+	String level = scanner.nextLine();
+	 
+		if(level = "Easy"){
+			System.out.print("Here Goes");
+	
+		else(level = "Hard"){
+			System.out.print("Alright then");
+		}
+	
+		int questions = 10;
+
+		
+	
+}
+}
