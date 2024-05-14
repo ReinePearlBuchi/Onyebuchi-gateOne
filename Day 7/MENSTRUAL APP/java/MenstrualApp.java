cd4989beacd63aeb8f6771eacf49483b19430980
Ekwutozia Onyebuchi Maureen <165788844+ReinePearlBuchi@users.noreply.github.com>
2024-05-14 14:33:14 +0100

    gateOne

--- Day 7/MENSTRUAL APP/java/MenstrualApp.java
+++ Day 7/MENSTRUAL APP/java/MenstrualApp.java
@@ -1,52 +1,51 @@
+import java.time.LocalDateTime;
 import java.util.Scanner;
 	public class MenstrualApp{
-	public static void main(String[] args){
+		public static void main(String[] args){
 
 
-	Scanner scanner = new Scanner(System.in);
-	System.out.print("Welcome to Red Robot Calendar! /n What is your Name? ");
-	String userinput = scanner.nextLine();
-
-	System.out.print("Hi!" + userinput );
-
-	System.out.println("Choose from the following to Describe how youre feeling today " );
-	System.out.println("1. Bright and cheery");
-	System.out.println("2. Grumpy and jumpy");
-	System.out.println("3. in sifia pain" )
-
-
-	choice = userInput.nextInt();
-
-	if(choice == 1 ) {
-		choice = Bright and Cheery;
-	} else if (choice == 2 ) {
-		choice = Grumpy and sad;
-	} else if(choice == 3) {
-		choice == in sifia pain;
-	} else if (choice  > 3 || choice < 1) {
-		System.out.printnln("Try again. " );
-		 choice = -1;
-		in my feelings();
+		Scanner scanner = new Scanner(System.in);
 		
-	}	
+		System.out.print("Welcome to Red Robot Calendar! \n What is your Name? ");
+		String userinput = scanner.nextLine();
+
+		System.out.print("Hi!" + userinput );
+
+		String prompt = """
+		\nDescribe how you are feeling today
+		"1" -> Bright and Cheery
+		"2" -> Grumpy and jumpy
+		"3" -> In sifia pain
+		""";
+	
+		Switch (prompt) {
+		case 1 :
+		System.out.print( "Bright and Cheery");
+		break ;
+		case 2 :
+		System.out.print( "Grumpy and jumpy");
+		break ;
+		case 3 :
+		System.out.print( "In sifia pain");
+		break ;
+		default :
+		System.out.print( "Invalid Input");
+		}
+
+		System.out.println("Saving" + ">".repeat(20));
+			System.out.println("Saved Successfully");
+
+
+
+		}
+		
+
+		
+			}
+
+		}
+
 
 
 	
-	}
-
 	
-
-	}
-
-
-	private static Scanner scanner = new Scanner(System.in);
-
-	private static int count A1 = 0;
-	private static int count B1 = 0;
-	private static int count A2 = 0;
-	private static int count B2 = 0;
-	private static int count A3 = 0;
-	private static int count B3 = 0;
-
-
-}
