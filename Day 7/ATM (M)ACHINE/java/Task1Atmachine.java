969975bc35e65129bcf24514120a191a5eb80c44
Ekwutozia Onyebuchi Maureen <165788844+ReinePearlBuchi@users.noreply.github.com>
2024-05-15 17:47:23 +0100

    gateOne

--- Day 7/ATM (M)ACHINE/java/Task1Atmachine.java
+++ Day 7/ATM (M)ACHINE/java/Task1Atmachine.java
@@ -1,57 +1,93 @@
 import java.util.Scanner;
 	public class Task1Atmachine{
+		private static int balance ;
+		private static int amountDeposit;
+
 		public static void main(String[] args){
+		
 
 		Scanner scanner = new Scanner(System. in);
 
-
 		display();
-		System.out.print("Choose an option ");
+		System.out.println("Choose an option ");
 		int option = scanner.nextInt();
 
-		switch(options){
+		switch(option){
 		case 0: 
-		System.out.println("Enter your First Name:  ");
+		scanner.nextLine();
+
+		System.out.print("Enter your First Name:  ");
 		String firstName = scanner.nextLine();
 	
-		System.out.println("Enter your Last Name:  ");
+		System.out.print("Enter your Last Name:  ");
 		String lastName = scanner.nextLine();
 
 		System.out.println("Insert Pin: ");
 		int pinInsert = scanner.nextInt();
 
-		System.out.println("Your Account has been Succssfully Created! \n Your Name is " firstName + lastName );
+		System.out.println("Your Account has been Succssfully Created! \n Your Name is " + firstName + " " +lastName );
+		System.out.println("Saving" + ">".repeat(20));
 		break;
+		
+			
+
+
 
 
 		case 1:
-		System.out.println("Would you like to close Your Account? ");
+		System.out.println("Would you like to close Your Account?  yes/no");
+		String closeAccount = scanner.nextLine();
+
 	
 		System.out.println("What are your reasons why? ");
+		String reasonForClosure = scanner.nextLine();
 
 
-		System.out.println("");
+		System.out.print("Account Closed! Successfully!");
+		System.out.println("Saving" + ">".repeat(20));
 		break;
 
 
 		case 2:
-		System.out.print("Would you like to deposit  ");
+			
+			System.out.print("Would you like to deposit  ");
+			String question = scanner.nextLine();
 
-		System.out.print("How much would you like to deposit?  Enter Amount:  ");
+			System.out.print("How much would you like to deposit?  Enter Amount:  ");
+			int amountDeposit = scanner.nextInt();
+			deposit(amountDeposit);
 
-		System.out.print("Savings or Current account");
+			System.out.print("Savings or Current account");
+			String typeOfAccount = scanner.nextLine();
+
+			System.out.println("Transaction Successful");
+			System.out.println("Saving" + ">".repeat(20));
+
 		break;
+
 
 		case 3:
-		System.out.println("How much would you like to withdraw?  Enter Amount:");
+			System.out.println("How much would you like to withdraw?  Enter Amount:");
+			int amountToWithdraw = scanner.nextInt();
+			withdraw(amountToWithdraw);
+
+
+			System.out.println("Transaction Successful");
+			System.out.println("Saving" + ">".repeat(20));
+
 		break;
 
+
 		case 4:
-		System.out.print("The balance of your account is");
+			System.out.print("The balance of your account is"  + amountDeposit  );
+
+
 		break;
 
 		case 5:
-		System.out.print("Would you like to change Your pin? ");	
+			System.out.print("Would you like to change Your pin? ");
+		int changePin = scanner.nextInt();
+	
 		break;
 
 		default:
@@ -64,11 +100,11 @@ import java.util.Scanner;
 
 		}
 
-	public static void createId(){
-	System.out.print("Creating your ID for yourr Account===>  ");
-	}
+		public static void createId(){
+		System.out.print("Creating your ID for your Account===>  ");
+		}
 
-	public static void display(){
+		public static void display(){
 			String Options= """
 				Press
 		0-> Open Account
@@ -80,7 +116,31 @@ import java.util.Scanner;
 		""";
 		System.out.print(Options);
 
-	}	
+		}	
 
+		public static int deposit(int num1){
+			int amount = 0;
+			 balance = 0;
+
+			 return balance + amount;
+			
+
+		}
+
+		public static int withdraw(int num1){
+			int amount = 0;
+			int withdraw = 0;
+			
+
+			return balance + withdraw;
+		}
+
+		
+		public static int balance(int num1){
+			int amount = 0;
+			 balance = 0;
+			return balance;
+
+		}
 	
 }
